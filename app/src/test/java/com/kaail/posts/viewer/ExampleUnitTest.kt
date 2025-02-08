88d8b79199454f868c53d4c9fef20f6f691544a3
Saul Emmanuel Flores Bernal <kaail.bernal@gmail.com>
2025-02-07 22:10:11 -0600

    Implement ktlint to format code according to standard rules (#1)
    * Add files to ignore
    
    * Add ktlint plugin and its configs
    
    * Format code according to standard rules

--- app/src/test/java/com/kaail/posts/viewer/ExampleUnitTest.kt
+++ app/src/test/java/com/kaail/posts/viewer/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.kaail.posts.viewer
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
